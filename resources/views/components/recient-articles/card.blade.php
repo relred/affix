a2fe8f7191fa5a119a83724fd6aff0b251c4feff
Omar <dev@relred.com>
2024-08-20 22:08:36 -0700

    Solved minor visual bug when viewing on chrome

--- resources/views/components/recient-articles/card.blade.php
+++ resources/views/components/recient-articles/card.blade.php
@@ -1,11 +1,12 @@
 <li class="mt-4">
     <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border bg-white rounded-md" href="{{ route('articles.show', $slug) }}">
-        <img src="{{ $imageUrl }}" alt="" class="bd-placeholder-img" width="100%" height="auto">
-        <div class="col-lg-8 p-2">
-            <h6 class="mb-0">{{ $title }}</h6>
-            <small class="text-body-secondary">{{ $date }}</small>
-            <p class="m-1">{{ $description }}</p>
-        </div>
+      <div class="flex-shrink-0" style="width: 200px; height: 150px; overflow: hidden;">
+        <img src="{{ $imageUrl }}" alt="" class="bd-placeholder-img" style="width: 100%; height: 100%; object-fit: cover;">
+      </div>
+      <div class="col-lg-8 p-2">
+        <h6 class="mb-0">{{ $title }}</h6>
+        <small class="text-body-secondary">{{ $date }}</small>
+        <p class="m-1">{{ $description }}</p>
+      </div>
     </a>
-    
-</li>
+  </li>
