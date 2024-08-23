4616a112118c173c859efa3585ccc198d2215e28
Omar <dev@relred.com>
2024-08-23 14:55:27 -0700

    Changed cover component from static cover to carousel

---
+++ resources/views/components/carousel/body.blade.php
@@ -0,0 +1,21 @@
+<div id="featuredArticlesCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      @foreach($feed as $index => $article)
+        <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
+          <x-cover 
+          slug="{{ $article->slug }}"
+          imageUrl="{{ $article->imageUrl }}"
+          title="{{ $article->title }}"
+          description="{{ $article->description }}"/>
+        </div>
+      @endforeach
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#featuredArticlesCarousel" data-bs-slide="prev" style="width: 6%;">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Anterior</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#featuredArticlesCarousel" data-bs-slide="next" style="width: 6%">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Siguiente</span>
+    </button>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,27 +10,7 @@
 
 <main class="container">
 
-  <x-cover 
-  slug="{{ $cover->slug }}"
-  imageUrl="{{ $cover->imageUrl }}"
-  title="{{ $cover->title }}"
-  description="{{ $cover->description }}"/>
-
-{{--   <div class="row mb-2">
-    <x-news-card
-      imageUrl="https://imagenes.elpais.com/resizer/v2/O6ZLI7KQHBESHI62JP7V6FWG5E.jpg?auth=b460901d76d27e1a948f873845a3f51499f201f666e09828863b63f08045c07e&width=1200"
-      title="Elecciones en Venezuela: ¿Qué países se posicionan contra la reelección de Maduro?"
-      description="No son pocos los países que actualmente no reconocen a Nicolás Maduro como el siguiente presente de Venezuela"
-      date="02-07-2024"
-      category="Internacional"/>
-
-      <x-news-card
-      imageUrl="https://imagenes.elpais.com/resizer/v2/O6ZLI7KQHBESHI62JP7V6FWG5E.jpg?auth=b460901d76d27e1a948f873845a3f51499f201f666e09828863b63f08045c07e&width=1200"
-      title="Elecciones en Venezuela: ¿Qué países se posicionan contra la reelección de Maduro?"
-      description="No son pocos los países que actualmente no reconocen a Nicolás Maduro como el siguiente presente de Venezuela"
-      date="02-07-2024"
-      category="Internacional"/>
-  </div> --}}
+<x-carousel.body :$feed/>
 
   <div style="margin: auto; max-width:900px;">
     <div class="">
