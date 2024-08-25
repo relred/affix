b2ae70a3e6e3a7708c6897c6145a3de8654df9ce
Omar <dev@relred.com>
2024-08-24 23:49:12 -0700

    Fix: Corrected spacing left on article meta tags

--- resources/views/article-page.blade.php
+++ resources/views/article-page.blade.php
@@ -5,17 +5,11 @@
 
 @endsection
 
-@section('title')
-    {{ $article->title }}
-@endsection
+@section('title'){{ $article->title }}@endsection
 
-@section('description')
-    {{ $article->description }}
-@endsection
+@section('description'){{ $article->description }}@endsection
 
-@section('imageUrl')
-    {{ $article->imageUrl }}
-@endsection
+@section('imageUrl'){{ $article->imageUrl }}@endsection
 
 @section('videoUrl')
 @endsection
