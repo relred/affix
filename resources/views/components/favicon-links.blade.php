b211c3c745481989a5be2e0cc80c533610e4234b
Omar <dev@relred.com>
2024-08-26 13:52:01 -0700

    Feat: implement favicon and manifest links in layouts

--- resources/views/components/favicon-links.blade.php
+++ resources/views/components/favicon-links.blade.php
@@ -1,5 +1,5 @@
-<link rel="icon" type="image/x-icon" href="https://res.cloudinary.com/de6hiq5n4/image/upload/v1724703558/affix/assets/icons/favicon.ico">
-<link rel="apple-touch-icon" sizes="180x180" href="https://res.cloudinary.com/de6hiq5n4/image/upload/v1724703609/affix/assets/icons/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="https://res.cloudinary.com/de6hiq5n4/image/upload/v1724703609/affix/assets/icons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="https://res.cloudinary.com/de6hiq5n4/image/upload/v1724703609/affix/assets/icons/favicon-16x16.png">
+<link rel="icon" type="image/x-icon" href="https://res.cloudinary.com/de6hiq5n4/image/upload/f_auto,q_auto/v1/affix/assets/icons/favicon">
+<link rel="apple-touch-icon" sizes="180x180" href="https://res.cloudinary.com/de6hiq5n4/image/upload/f_auto,q_auto/v1/affix/assets/icons/apple-touch-icon">
+<link rel="icon" type="image/png" sizes="32x32" href="https://res.cloudinary.com/de6hiq5n4/image/upload/f_auto,q_auto/v1/affix/assets/icons/favicon-32x32">
+<link rel="icon" type="image/png" sizes="16x16" href="https://res.cloudinary.com/de6hiq5n4/image/upload/f_auto,q_auto/v1/affix/assets/icons/favicon-16x16">
 <link rel="manifest" href="{{ asset('site.webmanifest') }}">
