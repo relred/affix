f5a3c671998770fd6ed8da0a9d694acfc89a1355
Omar <dev@relred.com>
2024-08-28 20:08:28 -0700

    style: updated favicon icons in links component and site manifest

--- resources/views/components/favicon-links.blade.php
+++ resources/views/components/favicon-links.blade.php
@@ -1,5 +1,5 @@
-<link rel="icon" type="image/x-icon" href="https://res.cloudinary.com/de6hiq5n4/image/upload/f_auto,q_auto/v1/affix/assets/icons/favicon">
-<link rel="apple-touch-icon" sizes="180x180" href="https://res.cloudinary.com/de6hiq5n4/image/upload/f_auto,q_auto/v1/affix/assets/icons/apple-touch-icon">
-<link rel="icon" type="image/png" sizes="32x32" href="https://res.cloudinary.com/de6hiq5n4/image/upload/f_auto,q_auto/v1/affix/assets/icons/favicon-32x32">
-<link rel="icon" type="image/png" sizes="16x16" href="https://res.cloudinary.com/de6hiq5n4/image/upload/f_auto,q_auto/v1/affix/assets/icons/favicon-16x16">
+<link rel="icon" type="image/x-icon" href="https://res.cloudinary.com/de6hiq5n4/image/upload/f_auto,q_auto/v1/affix/assets/web-icons/favicon">
+<link rel="apple-touch-icon" sizes="180x180" href="https://res.cloudinary.com/de6hiq5n4/image/upload/f_auto,q_auto/v1/affix/assets/web-icons/apple-touch-icon">
+<link rel="icon" type="image/png" sizes="32x32" href="https://res.cloudinary.com/de6hiq5n4/image/upload/f_auto,q_auto/v1/affix/assets/web-icons/favicon-32x32">
+<link rel="icon" type="image/png" sizes="16x16" href="https://res.cloudinary.com/de6hiq5n4/image/upload/f_auto,q_auto/v1/affix/assets/web-icons/favicon-16x16">
 <link rel="manifest" href="{{ asset('site.webmanifest') }}">
