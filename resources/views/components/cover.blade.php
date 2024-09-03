bc23e05f1fd0af7f93de174eef3d2a6950c2ab25
Omar <dev@relred.com>
2024-09-02 17:58:30 -0700

    feat: reworked Cover component to simplify displayed information

--- resources/views/components/cover.blade.php
+++ resources/views/components/cover.blade.php
@@ -1,41 +1,52 @@
 <style>
     .card-cover {
-        aspect-ratio: 16 / 14;  /* Default for mobile screens */
+      aspect-ratio: 16 / 14; /* Default for mobile screens */
     }
-
-    @media (min-width: 992px) {  /* lg screens and up */
-        .card-cover {
-            aspect-ratio: 16 / 7;
+    @media (min-width: 992px) { /* lg screens and up */
+      .card-cover {
+        aspect-ratio: 16 / 7;
+      }
+    }
+    .card-cover h1 {
+      font-size: clamp(1.5rem, 4vw, 2.5rem);
+      line-height: 1.2;
+      max-height: 3.6em;
+      overflow: hidden;
+      display: -webkit-box;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
+    }
+    .card-cover .description-text {
+      font-size: clamp(1rem, 2vw, 1.25rem);
+      max-height: 4.5em;
+      overflow: hidden;
+      display: -webkit-box;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
+    }
+    @media (min-width: 768px) {
+        .card-cover .card-title {
+        padding-left: 3rem !important;
+        padding-right: 3rem !important;
         }
     }
-
-    .card-cover h1 {
-        font-size: clamp(1.5rem, 4vw, 2.5rem);
-        line-height: 1.2;
-        max-height: 3.6em;
-        overflow: hidden;
-        display: -webkit-box;
-        -webkit-line-clamp: 3;
-        -webkit-box-orient: vertical;
+    @media (min-width: 992px) {
+        .card-cover .card-title {
+        padding-left: 5rem !important;
+        padding-right: 5rem !important;
+        }
     }
-    
-    .card-cover .description-text {
-        font-size: clamp(1rem, 2vw, 1.25rem);
-        max-height: 4.5em;
-        overflow: hidden;
-        display: -webkit-box;
-        -webkit-line-clamp: 3;
-        -webkit-box-orient: vertical;
+    @media (min-width: 1200px) {
+        .card-cover .card-title {
+        padding-left: 7rem !important;
+        padding-right: 7rem !important;
+        }
     }
-</style>
-
-<div class="card-cover p-4 p-md-5 mb-4 rounded text-body-emphasis position-relative" style="background-image: url('{{ $imageUrl }}'); background-size: cover; background-position: center; overflow: hidden;">
-    <a href="{{ route('articles.show', $slug) }}" class="text-decoration-none">
-        <div class="position-absolute top-0 start-0 w-100 h-100" style="background: linear-gradient(to right, rgba(0,0,0,0.9) 0%, rgba(0,0,0,0.5) 50%, rgba(0,0,0,0) 100%);"></div>
-        <div class="col-lg-6 px-0 position-relative h-100 d-flex flex-column justify-content-center">
-            <h1 class="ml-2 display-4 fst-italic text-white">{{ $title }}</h1>
-            <p class="lead my-3 text-white description-text">{{ $description }}</p>
-            <p class="lead mb-0"><a href="{{ route('articles.show', $slug) }}" class="text-white fw-bold">Continuar Leyendo...</a></p>
-        </div>
-    </a>
-</div>
+   </style>
+   
+    <div class="card-cover p-4 p-md-5 mb-4 rounded text-body-emphasis position-relative" style="background-image: url('{{ $imageUrl }}'); background-size: cover; background-position: center; overflow: hidden;">
+        <a href="{{ route('articles.show', $slug) }}" class="text-decoration-none">
+        <div class="position-absolute top-0 start-0 w-100 h-100" style="background: linear-gradient(to top, rgba(0,0,0,0.9) 0%, rgba(0,0,0,0.5) 50%, rgba(0,0,0,0) 100%);"></div>
+            <h1 class="card-title fst-italic text-white px-4 pb-3 mb-3 position-absolute bottom-0 start-0 end-0">{{ $title }}</h1>
+        </a>
+    </div>
